112d1c83f4d40c644a171289658d02599e3c6fc8
Jwan <gwan17_7@yahoo.com>
2021-04-23 23:59:13 +0200

    make up

--- Scheduler/altinget_scheduler.py
+++ Scheduler/altinget_scheduler.py
@@ -3,23 +3,22 @@ import time
 from news_scraping.altinget import scrape_altinget
 
 
-class altinget_scheduler:
+def run_altinget():
+    try:
+        print('Altinget is running every 20 seconds ... ')
+        scrape_altinget(2)
 
-    def run_altinget(self):
-        try:
-            print('Altinget is running every 20 seconds ... ')
-            scrape_altinget(2)
+    except Exception as e:
+        print('Problem running Altinget scheduler: ', e)
 
-        except Exception as e:
-            print('Problem running Altinget scheduler: ', e)
 
-    schedule.every(10).seconds.do(run_altinget)
+schedule.every(10).seconds.do(run_altinget)
 
-    while True:
-        try:
-            run_altinget()
+while True:
+    try:
+        run_altinget()
 
-            schedule.run_pending()
-            time.sleep(1)
-        except Exception as e:
-            print(e)
+        schedule.run_pending()
+        time.sleep(1)
+    except Exception as e:
+        print(e)
