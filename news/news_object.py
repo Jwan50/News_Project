7c5ef9ac2e4969d8f66b0fbe58c6c14b2be217b6
Jwan <gwan17_7@yahoo.com>
2021-05-25 01:44:18 +0200

    Creating pipline

--- news/news_object.py
+++
@@ -1,9 +0,0 @@
-
-
-class news_object:
-    def __init__(self):
-        self.category = None
-        self.headline = None
-        self.content = None
-        self.date = None
-        self.provider = None
