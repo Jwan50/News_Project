838296de643cc010813f8dae25b4d768287cd8f0
Jwan <gwan17_7@yahoo.com>
2021-06-01 07:47:33 +0200

    Cleanup code and comments added

--- radio_scrapers/p3_scraper.py
+++ radio_scrapers/p3_scraper.py
@@ -12,38 +12,38 @@ data_name = 'P3_playlist'
 
 
 def scrap_P3(scrape_back_days, runType):
-    urlbase = 'https://www.dr.dk/playlister/'
-    urlbase_name = urlbase + radioName + "/"
-    today = datetime.datetime.now()
-    run_end = today - datetime.timedelta(days=scrape_back_days)
+    urlbase = 'https://www.dr.dk/playlister/'                           # Common link between P3 and P4
+    urlbase_name = urlbase + radioName + "/"                            # Adding the radio name to the common link
+    today = datetime.datetime.now()                                     # The time now
+    run_end = today - datetime.timedelta(days=scrape_back_days)         # Go to where we need to scape
 
     while run_end <= today:
         try:
-            scrap_start = today.strftime("%Y,%m,%d")
-            url = urlbase_name + scrap_start
-            urltxt = requests.get(url)
-            urltxt = urltxt.content
-            soup = bs.BeautifulSoup(urltxt, 'lxml')
+            scrap_start = today.strftime("%Y,%m,%d")                    # Converting to string format with only Y - m - d
+            url = urlbase_name + scrap_start                            # Adding the date of scraping to the url to target the right webpage of that date
+            urltxt = requests.get(url)                                  # Reuests to get Json file
+            urltxt = urltxt.content                                     # Target the content property of the comming Json
+            soup = bs.BeautifulSoup(urltxt, 'lxml')                     # To reconstruct the Json file to be as html tree for easier browsing
             ids_group = soup.findAll('div', {'class': 'span-3'})
-            links = ids_group[0]('a')
+            links = ids_group[0]('a')                                   # Here we found all the list of links based on each program of the day
             for link in reversed(links):
-                link_url = link['href']
-                url_new = 'https://www.dr.dk' + link_url
+                link_url = link['href']                                 # Get the link of each program
+                url_new = 'https://www.dr.dk' + link_url                # Building a new url to go to each link seperately
                 urltxt_new = requests.get(url_new)
                 urltxt_new = urltxt_new.content.decode("utf-8", "ignore")
                 soup = bs.BeautifulSoup(urltxt_new, 'lxml')
-                songs = soup.findAll('li', {'class': 'track'})
+                songs = soup.findAll('li', {'class': 'track'})          # Playlists are here
                 if not songs:
                     continue
                 else:
                     for song in reversed(songs):
                         try:
-                            artist = p3_getArtist(song)
-                            title = p3_getTitle(song)
-                            dt = p3_getDate(song, soup, today, link_url, linkName)
-                            radio_gathering_data(artist, title, dt, runType, data_name)
+                            artist = p3_getArtist(song)                                         # Call p3_getArtist function
+                            title = p3_getTitle(song)                                           # Call p3_getTitle function
+                            dt = p3_getDate(song, soup, today, link_url, linkName)              # Call p3_getDate function
+                            radio_gathering_data(artist, title, dt, runType, data_name)         # Gathering all together
                         except Exception as e:
                             print(e)
         except Exception as e:
             print(e)
-        today = today - datetime.timedelta(days=1)
+        today = today - datetime.timedelta(days=1)                                              # Go backward one day
